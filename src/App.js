83f28df050c13ddfbec87054f3b51d3f84ac87b0
sjb-ch1mp <samuel.brookes@hotmail.com>
2022-06-26 21:28:00 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/logo.png';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-input-container">
+          <input type="text" className="App-input" placeholder="Enter a URL or drop a file here to get started..."/>
+          <button className="App-input-button">Git shuckin'</button>
+        </div>
+        <div>
+          <a className="App-footer" href="https://github.com/sjb-ch1mp" target="_blank">github.com/sjb-ch1mp</a>
+        </div>
       </header>
     </div>
   );
